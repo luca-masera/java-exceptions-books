79e0bc03c3f95e873e648d94d2a2fd0d949481be
luca-masera <lucamasera1995@gmail.com>
2024-04-03 17:36:44 +0200

    file con book

--- src/org/experis/exerciseBook/Library.java
+++ src/org/experis/exerciseBook/Library.java
@@ -1,5 +1,9 @@
 package org.experis.exerciseBook;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Library {
@@ -8,9 +12,38 @@ public class Library {
 
         System.out.print("Quanti libri vuoi? ");
         int numLibri = scan.nextInt();
-
+        scan.nextLine();
         Book[] books = new Book[numLibri];
-
+        for (int i = 0; i < numLibri; i++) {
+            try {
+                System.out.println("Inserisci i dati del libro " + (i + 1) + ":");
+                System.out.print("Titolo: ");
+                String titolo = scan.nextLine();
+                System.out.print("Numero di pagine: ");
+                int numPagine = scan.nextInt();
+                scan.nextLine();
+                System.out.print("Autore: ");
+                String autore = scan.nextLine();
+                System.out.print("Editore: ");
+                String editore = scan.nextLine();
+                Book book = new Book(titolo, numPagine, autore, editore);
+                books[i] = book;
+            }
+            catch (IllegalArgumentException e) {
+            System.out.println("Errore, qualcosa è andato storto: " + e.getMessage() + " Riprova.");
+            i--;
+            }
+        }
         scan.close();
+
+        try (Scanner fileScanner = new Scanner(new File("./resources/libri.txt"))) {
+            System.out.println("Dati dei libri letti dal file:");
+            while (fileScanner.hasNextLine()) {
+                System.out.println(fileScanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File non trovato: " + e.getMessage());
+
+        }
     }
 }
